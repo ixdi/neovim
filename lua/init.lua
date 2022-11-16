fee15c542b444afbe6de1fef7fadab4d922cd095
Sergi Cortés <sergi.cortes@nextmol.com>
2022-11-16 09:09:24 +0100

    Fix null-ls

--- lua/init.lua
+++ lua/init.lua
@@ -250,9 +250,9 @@ require('lspconfig')['cssls'].setup {
 require('lspconfig')['dockerls'].setup {
   capabilities = capabilities
 }
-require('lspconfig')['eslint'].setup {
-  capabilities = capabilities
-}
+-- require('lspconfig')['eslint'].setup {
+--   capabilities = capabilities
+-- }
 require('lspconfig')['emmet_ls'].setup {
   capabilities = capabilities
 }
@@ -306,10 +306,6 @@ require("zen-mode").setup({})
 local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.autopep8,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.diagnostics.pylint,
-    null_ls.builtins.code_actions.eslint_d,
   },
 })
