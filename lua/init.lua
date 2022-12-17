bcd82d15ef8080fe632cef41866dfe167bb7bcef
Sergi Cortés <sergi.cortes@nextmol.com>
2022-12-17 11:49:53 +0100

    Add tsserver to have definition jump'

--- lua/init.lua
+++ lua/init.lua
@@ -3,79 +3,68 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-treesitter.configs").setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "html", "css", "scss", "json", "typescript", "yaml", "python", "toml", "glimmer",
-    "jsdoc", "tsx", "lua", "markdown", "bash", "dockerfile", "comment" },
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        "javascript", "html", "css", "scss", "json", "typescript", "yaml",
+        "python", "toml", "glimmer", "jsdoc", "tsx", "lua", "markdown", "bash",
+        "dockerfile", "comment"
+    },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "" },
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = {""},
 
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
 
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "" },
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        disable = {""},
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-  incremental_selection = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  }
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false
+    },
+    incremental_selection = {enable = true},
+    indent = {enable = true},
+    rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil -- Do not enable for files with more than n lines, int
+        -- colors = {}, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
+    }
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
 parser_config.glimmer = {
-  filetype = "handlebars",
-  used_by = {
-    "handlebars",
-    "html.handlebars",
-    "spacebars",
-    "html"
-  }
+    filetype = "handlebars",
+    used_by = {"handlebars", "html.handlebars", "spacebars", "html"}
 }
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "s", action = "split" },
-        { key = "v", action = "vsplit" },
-        { key = "sy", action = "system_open" },
-        { key = "o", action = "edit_no_picker" },
-      },
+    sort_by = "case_sensitive",
+    view = {
+        adaptive_size = true,
+        mappings = {
+            list = {
+                {key = "s", action = "split"}, {key = "v", action = "vsplit"},
+                {key = "sy", action = "system_open"},
+                {key = "o", action = "edit_no_picker"}
+            }
+        }
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    renderer = {group_empty = true},
+    filters = {dotfiles = true}
 })
 
 require('lualine').setup()
@@ -83,42 +72,41 @@ require('Comment').setup()
 require('hop').setup()
 -- Hop keybindings
 vim.api.nvim_set_keymap('', 'f',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
-  , {})
+                        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+                        {})
 vim.api.nvim_set_keymap('', 'F',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
-  , {})
+                        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
+                        {})
 vim.api.nvim_set_keymap('', 't',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>"
-  , {})
+                        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
+                        {})
 vim.api.nvim_set_keymap('', 'T',
-  "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>"
-  , {})
-
+                        "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>",
+                        {})
 
 require("nvim-autopairs").setup {}
 require('nvim-ts-autotag').setup()
 
 -- Use xsel instead of xclip
 vim.g.clipboard = {
-  name = "xsel_override",
-  copy = {
-    ["+"] = "xsel --input --clipboard",
-    ["*"] = "xsel --input --primary",
-  },
-  paste = {
-    ["+"] = "xsel --output --clipboard",
-    ["*"] = "xsel --output --primary",
-  },
-  cache_enabled = 1,
+    name = "xsel_override",
+    copy = {
+        ["+"] = "xsel --input --clipboard",
+        ["*"] = "xsel --input --primary"
+    },
+    paste = {
+        ["+"] = "xsel --output --clipboard",
+        ["*"] = "xsel --output --primary"
+    },
+    cache_enabled = 1
 }
 
 require("yanky").setup({})
 -- Yank keymaps
-vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
-vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+vim.keymap.set({"n", "x"}, "p", "<Plug>(YankyPutAfter)")
+vim.keymap.set({"n", "x"}, "P", "<Plug>(YankyPutBefore)")
+vim.keymap.set({"n", "x"}, "gp", "<Plug>(YankyGPutAfter)")
+vim.keymap.set({"n", "x"}, "gP", "<Plug>(YankyGPutBefore)")
 vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
 vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
 vim.keymap.set("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)")
@@ -136,13 +124,19 @@ require('gitsigns').setup()
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "pyright", "tsserver", "cssls", "eslint", "sumneko_lua", "emmet_ls", "html", "jsonls", "marksman", },
-  automatic_installation = true,
+    ensure_installed = {
+        "pyright", "tsserver", "cssls", "eslint", "sumneko_lua", "emmet_ls",
+        "html", "jsonls", "marksman"
+    },
+    automatic_installation = true
 })
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and
+               vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col,
+                                                                          col)
+                   :match("%s") == nil
 end
 
 -- lspkind plugin to be loaded before cmp
@@ -152,94 +146,82 @@ local lspkind = require('lspkind')
 local cmp = require 'cmp'
 
 cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<esc>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<Tab>'] = function(fallback)
-      if (cmp.visible() and has_words_before()) then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    { name = 'nvim_lua' },
-    { name = 'snippy' }, -- For snippy users.
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'cmdline' },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-    })
-  }
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<esc>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({select = true}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<Tab>'] = function(fallback)
+            if (cmp.visible() and has_words_before()) then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end
+    }),
+    sources = cmp.config.sources({
+        {name = 'nvim_lsp'}, -- { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        {name = 'nvim_lua'}, {name = 'snippy'}, -- For snippy users.
+        {name = 'buffer'}, {name = 'path'}, {name = 'cmdline'}
+    }),
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...' -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        })
+    }
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+    sources = cmp.config.sources({
+        {name = 'cmp_git'} -- You can specify the `cmp_git` source if you were installed it.
+    }, {{name = 'buffer'}})
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+cmp.setup.cmdline({'/', '?'}, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {{name = 'buffer'}}
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp
+                                                                      .protocol
+                                                                      .make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['tsserver'].setup {
---   capabilities = capabilities
--- }
+require('lspconfig')['tsserver'].setup {capabilities = capabilities}
 -- require('lspconfig')['pyright'].setup {
 --   capabilities = capabilities
 -- }
@@ -277,61 +259,43 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 -- Telescope
 require("telescope").load_extension("yank_history")
 
-require "lsp_signature".setup()
+require"lsp_signature".setup()
 
-require 'colorizer'.setup()
+require'colorizer'.setup()
 
 require("filetype").setup({})
 
 require("indent_blankline").setup {
-  -- for example, context is off by default, use this to turn it on
-  show_current_context = false,
-  show_current_context_start = false,
+    -- for example, context is off by default, use this to turn it on
+    show_current_context = false,
+    show_current_context_start = false
 }
 
 require('neogen').setup({
-  languages = {
-    lua = {
-      template = {
-        annotation_convention = "emmylua"
-      }
-    },
-    python = {
-      template = {
-        annotation_convention = "reST"
-      }
-    },
-    typescript = {
-      template = {
-        annotation_convention = "jsdoc"
-      }
-    },
-  }
+    languages = {
+        lua = {template = {annotation_convention = "emmylua"}},
+        python = {template = {annotation_convention = "reST"}},
+        typescript = {template = {annotation_convention = "jsdoc"}}
+    }
 })
 
 require("zen-mode").setup({})
 
 local null_ls = require("null-ls")
 null_ls.setup({
-  sources = {
-    null_ls.builtins.code_actions.eslint,
-    null_ls.builtins.code_actions.refactoring,
-    null_ls.builtins.diagnostics.ansiblelint,
-    null_ls.builtins.diagnostics.codespell,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.diagnostics.luacheck,
-    null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.diagnostics.pylint,
-    null_ls.builtins.diagnostics.stylelint,
-    null_ls.builtins.diagnostics.tidy,
-    null_ls.builtins.diagnostics.vint,
-    null_ls.builtins.diagnostics.zsh,
-    null_ls.builtins.formatting.autopep8,
-    null_ls.builtins.formatting.beautysh,
-    null_ls.builtins.formatting.eslint,
-    null_ls.builtins.formatting.fixjson,
-    null_ls.builtins.formatting.lua_format,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.hover.printenv,
-  },
+    sources = {
+        null_ls.builtins.code_actions.refactoring,
+        null_ls.builtins.diagnostics.ansiblelint,
+        null_ls.builtins.diagnostics.codespell,
+        null_ls.builtins.diagnostics.luacheck,
+        null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.diagnostics.pylint,
+        null_ls.builtins.diagnostics.stylelint,
+        null_ls.builtins.diagnostics.tidy, null_ls.builtins.diagnostics.vint,
+        null_ls.builtins.diagnostics.zsh, null_ls.builtins.formatting.autopep8,
+        null_ls.builtins.formatting.beautysh,
+        null_ls.builtins.formatting.fixjson,
+        null_ls.builtins.formatting.lua_format,
+        null_ls.builtins.formatting.prettierd, null_ls.builtins.hover.printenv
+    }
 })
