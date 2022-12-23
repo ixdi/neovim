7bb60f130487c6818eb49f0d57cc74862bcee53b
Sergi Cortés <sergi.cortes@nextmol.com>
2022-12-23 08:34:24 +0100

    Fix keymap

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,25 +1,48 @@
 local servers = {
-  "sumneko_lua", "cssls", "html", "tsserver", "pyright", "bashls", "jsonls",
-  "yamlls", "marksman", "ember", "emmet_ls"
+    "sumneko_lua", "cssls", "html", "tsserver", "pyright", "bashls", "jsonls",
+    "yamlls", "marksman", "ember", "emmet_ls"
 }
 
+-- INSTALLED in Mason by hand as we don't want some on lspconfig
+-- 'bash-language-server',
+-- 'beautysh',
+-- 'black',
+-- 'css-lsp',
+-- 'djlint',
+-- 'ember-language-server',
+-- 'emmet-ls',
+-- 'eslint-lsp',
+-- 'flake8',
+-- 'html-lsp',
+-- 'json-lsp',
+-- 'lua-language-server',
+-- 'markdownlint',
+-- 'marksman',
+-- 'prettier',
+-- 'pylint',
+-- 'pyright',
+-- 'stylua',
+-- 'typescript-language-server',
+-- 'yaml-language-server',
+-- 'yamlfmt',
+
 local settings = {
-  ui = {
-    border = "none",
-    icons = {
-      package_installed = "◍",
-      package_pending = "◍",
-      package_uninstalled = "◍"
-    }
-  },
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4
+    ui = {
+        border = "none",
+        icons = {
+            package_installed = "◍",
+            package_pending = "◍",
+            package_uninstalled = "◍"
+        }
+    },
+    log_level = vim.log.levels.INFO,
+    max_concurrent_installers = 4
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-  ensure_installed = servers,
-  automatic_installation = true
+    ensure_installed = servers,
+    automatic_installation = true
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
@@ -28,15 +51,15 @@ if not lspconfig_status_ok then return end
 local opts = {}
 
 for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities
-  }
+    opts = {
+        on_attach = require("user.lsp.handlers").on_attach,
+        capabilities = require("user.lsp.handlers").capabilities
+    }
 
-  server = vim.split(server, "@")[1]
+    server = vim.split(server, "@")[1]
 
-  local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-  if require_ok then opts = vim.tbl_deep_extend("force", conf_opts, opts) end
+    local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
+    if require_ok then opts = vim.tbl_deep_extend("force", conf_opts, opts) end
 
-  lspconfig[server].setup(opts)
+    lspconfig[server].setup(opts)
 end
--- lua/user/toggle-diagnostics.lua
+++ lua/user/toggle-diagnostics.lua
@@ -1,6 +1,7 @@
 vim.o.updatetime = 250
 -- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
-vim.keymap.set("n", "<leader>ee", ":lua vim.diagnostic.open_float(nil, {focus=false})<cr>", opts)
+vim.keymap.set("n", "<leader>ee",
+               ":lua vim.diagnostic.open_float(nil, {focus=false})<cr>")
 
-require('toggle_lsp_diagnostics').init({ virtual_text = false })
+require("toggle_lsp_diagnostics").init({virtual_text = false})
