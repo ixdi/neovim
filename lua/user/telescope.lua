66108eadf17579c6d2ba13be49c7359bc5712083
Sergi Cortés <sergi.cortes@nextmol.com>
2022-12-31 11:18:57 +0100

    Fix telescope ignore files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,15 +1,18 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then return end
 
-local actions = require "telescope.actions"
+local actions = require("telescope.actions")
 
-telescope.setup {
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-    file_ignore_patterns = { ".git/", "node_modules", ".meteor/" }
-  }
-}
+telescope.setup({
+    defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = {"smart"},
+        file_ignore_patterns = {
+            ".git", "node_modules", ".meteor", "public", "imports/externals",
+            "packages"
+        }
+    }
+})
 
 require("telescope").load_extension("yank_history")
