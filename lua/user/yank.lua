9f80092188387340435302ca3e52bfd05414e26d
Sergi Cortés <scortema@gmail.com>
2023-01-21 19:51:32 +0100

    Fix yank using xsel instead of xclip

--- lua/user/yank.lua
+++ lua/user/yank.lua
@@ -1,2 +1,15 @@
 require("yanky").setup({})
 
+vim.g.clipboard = {
+  name = "xsel_override",
+  copy = {
+    ["+"] = "xsel --input --clipboard",
+    ["*"] = "xsel --input --primary",
+  },
+  paste = {
+    ["+"] = "xsel --output --clipboard",
+    ["*"] = "xsel --output --primary",
+  },
+  cache_enabled = 1,
+}
+
