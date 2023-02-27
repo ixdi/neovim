5bacce8297f54a9e2a599554eec8791518f154b8
Sergi Cortés <sergi.cortes@nextmol.com>
2023-02-27 08:47:16 +0100

    Fix nvim-tree.config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,57 +1,53 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then return end
 
-local nvim_tree_config = require("nvim-tree.config")
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup {
-  update_focused_file = { enable = true, update_cwd = true },
-  renderer = {
-    group_empty = true,
-    root_folder_modifier = ":t",
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = ""
-        },
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "U",
-          deleted = "",
-          ignored = "◌"
+nvim_tree.setup({
+    update_focused_file = {enable = true, update_cwd = true},
+    renderer = {
+        group_empty = true,
+        root_folder_modifier = ":t",
+        icons = {
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    arrow_open = "",
+                    arrow_closed = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = ""
+                },
+                git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "U",
+                    deleted = "",
+                    ignored = "◌"
+                }
+            }
         }
-      }
-    }
-  },
-  diagnostics = {
-    enable = true,
-    show_on_dirs = true,
-    icons = { hint = "", info = "", warning = "", error = "" }
-  },
-  view = {
-    adaptive_size = true,
-    -- width = 30
-    side = "left",
-    mappings = {
-      list = {
-        { key = "s", action = "split" }, { key = "v", action = "vsplit" },
-        { key = "sy", action = "system_open" },
-        { key = "o", action = "edit_no_picker" }
-      }
-    }
-  },
-  filters = { dotfiles = true }
-}
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {hint = "", info = "", warning = "", error = ""}
+    },
+    view = {
+        adaptive_size = true,
+        -- width = 30
+        side = "left",
+        mappings = {
+            list = {
+                {key = "s", action = "split"}, {key = "v", action = "vsplit"},
+                {key = "sy", action = "system_open"},
+                {key = "o", action = "edit_no_picker"}
+            }
+        }
+    },
+    filters = {dotfiles = true}
+})
