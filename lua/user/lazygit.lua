7ceff828d71ab44731ee88818ba30c481668e8c2
Sergi Cortés <sergi.cortes@nextmol.com>
2023-05-26 08:17:53 +0200

    Fix (lazigit): option updated

--- lua/user/lazygit.lua
+++ lua/user/lazygit.lua
@@ -1,7 +1,7 @@
 vim.cmd("let g:lazygit_floating_window_winblend = 0") -- transparency of floating window
 vim.cmd("let g:lazygit_floating_window_scaling_factor = 0.9") -- scaling factor for floating window
 vim.cmd(
-    "let g:lazygit_floating_window_corner_chars = ['╭', '╮', '╰', '╯']") -- customize lazygit popup window corner characters
+    "let g:lazygit_floating_window_border_chars = ['╭', '╮', '╰', '╯']") -- customize lazygit popup window corner characters
 vim.cmd("let g:lazygit_floating_window_use_plenary = 1") -- use plenary.nvim to manage floating window if available
 vim.cmd("let g:lazygit_use_neovim_remote = 0") -- fallback to 0 if neovim-remote is not installed
 vim.cmd("let g:lazygit_use_custom_config_file_path = 0") -- config file path is evaluated if this value is 1
